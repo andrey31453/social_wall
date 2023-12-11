4e69040cfb8c2349a2f29dd75d7d125e9c1e9a52
kvisatchaderah <kvisatchaderah@gmail.com>
2023-12-11 18:32:05 +0300

    d

--- frontend/src/views/index/index.vue
+++ frontend/src/views/index/index.vue
@@ -1,6 +1,13 @@
 <script setup>
 import { banner, benefits, features } from '@components'
 import { ui_async } from '@ui'
+import { onMounted } from 'vue'
+
+const test = async () => {
+  const data = await fetch('/api')
+  console.log('data: ', data)
+}
+onMounted(test)
 </script>
 
 <template>
