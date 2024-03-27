69c67a5a5251869786876eb6a3c07fd36a23f0d9
andrey13453 <andrey13453@gmail.com>
2024-03-27 23:14:54 +0300

    d

--- client/src/shared/ui/the_wrapper.vue
+++ client/src/shared/ui/the_wrapper.vue
@@ -1,109 +1,109 @@
 <script setup>
 const props = defineProps({
-  between: {
-    type: Boolean,
-    default: false,
-  },
+	between: {
+		type: Boolean,
+		default: false,
+	},
 
-  center: {
-    type: Boolean,
-    default: false,
-  },
+	center: {
+		type: Boolean,
+		default: false,
+	},
 
-  end: {
-    type: Boolean,
-    default: false,
-  },
+	end: {
+		type: Boolean,
+		default: false,
+	},
 
-  column: {
-    type: Boolean,
-    default: false,
-  },
+	column: {
+		type: Boolean,
+		default: false,
+	},
 
-  fluid: {
-    type: Boolean,
-    default: false,
-  },
+	fluid: {
+		type: Boolean,
+		default: false,
+	},
 
-  p: {
-    type: Boolean,
-    default: false,
-  },
+	p: {
+		type: Boolean,
+		default: false,
+	},
 
-  px: {
-    type: Boolean,
-    default: false,
-  },
+	px: {
+		type: Boolean,
+		default: false,
+	},
 
-  py: {
-    type: Boolean,
-    default: false,
-  },
+	py: {
+		type: Boolean,
+		default: false,
+	},
 
-  gap: {
-    type: Boolean,
-    default: false,
-  },
+	gap: {
+		type: Boolean,
+		default: false,
+	},
 })
 
 // flex rules
 const get_justify = ({ between, center, end, column }) => {
-  if (column) return ''
-  if (between) return `justify-space-between`
-  if (center) return `justify-center`
-  if (end) return `justify-end`
+	if (column) return ''
+	if (between) return `justify-space-between`
+	if (center) return `justify-center`
+	if (end) return `justify-end`
 
-  return `justify-start`
+	return `justify-start`
 }
 
 const get_align = ({ center, end, column }) => {
-  if (column) return ''
+	if (column) return ''
 
-  return `align-center`
+	return `align-center`
 }
 
 const justify_class = get_justify({
-  between: props.between,
-  center: props.center,
-  end: props.end,
-  column: props.column,
+	between: props.between,
+	center: props.center,
+	end: props.end,
+	column: props.column,
 })
 
 const align_class = get_align({
-  center: props.center,
-  end: props.end,
-  column: props.column,
+	center: props.center,
+	end: props.end,
+	column: props.column,
 })
 </script>
 
 <template>
-  <div
-    :class="[
-      'the_wrapper',
-      'd-flex ga-2',
-      justify_class,
-      align_class,
-      {
-        'justify-center': center,
-        'justify-end': end,
-        'flex-column': column,
-        'h-100': fluid,
-        'pa-2': p,
-        'px-2': px,
-        'py-2': py,
-        'ga-3': gap,
-      },
-    ]"
-  >
-    <slot />
-  </div>
+	<div
+		:class="[
+			'the_wrapper',
+			'd-flex ga-2',
+			justify_class,
+			align_class,
+			{
+				'justify-center': center,
+				'justify-end': end,
+				'flex-column': column,
+				'h-100': fluid,
+				'pa-2': p,
+				'px-2': px,
+				'py-2': py,
+				'ga-3': gap,
+			},
+		]"
+	>
+		<slot />
+	</div>
 </template>
 
 <style lang="scss">
 @use '@styles/utils';
 
 .app_record {
-  width: 100%;
-  @include utils.font('mark');
+	width: 100%;
+	@include utils.font('mark');
 }
 </style>
